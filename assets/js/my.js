e4c432c00ea711d7edbbf4bc86f8f53b4756db1b
unknown <maligroaljhon@gmail.com>
2019-02-19 12:45:16 +0800

    update

--- assets/js/my.js
+++ assets/js/my.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
 	$('.fixed-action-btn').floatingActionButton();
+	$('.carousel.carousel-slider').carousel({
+	    fullWidth: true,
+	    indicators: true
+	  });
+      
 });
